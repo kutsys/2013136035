bede29f13ae0e27ee1ec1c8db101a643acd0bdf1
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-11 07:43:00 +0900

    Lab8,9

--- mproc2.c
+++ mproc2.c
@@ -3,39 +3,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-void test_funct(int count){
+void test_funct(int count,int i){
     time_t timer;
     struct tm* t;
     timer =time(NULL);
     t = localtime(&timer);
-    printf("  PID : %d\n",getpid());
-    printf(" time : %dh %dm %ds\n",t->tm_hour,t->tm_min,t->tm_sec);
+    printf("  p_thread : %d  ",i);
+    printf(" time : %dh %dm %ds  ",t->tm_hour,t->tm_min,t->tm_sec);
     printf("count : %d\n",count);
+    sleep(2);
 
 }
 
 
-void* subproc(void* num){
+void* subproc(void* i){
    int count =0;
-   while(count< *(int*)num){
+   int n = *(int *)i;
+   while(count< 20){
        srand(time(NULL));
        int r= rand()%10;
        sleep(r);
        count++;
-       test_funct(count);
-    }
-    pthread_exit("by2!");   
+       test_funct(count,n);
+    }   
 }
 
 int main()
 {
     int i=0;
+     int j=0;
+    int taskID[10];
     int res=0;
     pthread_t p_thread[10];
     void *thread_result;
     int num = 20;
     while(i<10){
-        res = pthread_create(&p_thread[i], NULL,subproc,(void*)&num);
+        taskID[i]=i;
+        res = pthread_create(&p_thread[i], NULL,subproc,(void*)&taskID[i]);
+	printf("thread create : %d\n",i);
         if(res !=0){
             perror("Thread creation failed");
             exit(EXIT_FAILURE);
@@ -43,14 +48,17 @@ int main()
 
         i++;
     }
-    i = 0;
-    while(i<10){
-       res =  pthread_join(p_thread[i], &thread_result);
+	
+    while(j<10){
+	
+
+       res =  pthread_join(p_thread[j], &thread_result);
        if(res !=0){
-           perror("Thread creation failed");
+           perror("Thread join failed");
            exit(EXIT_FAILURE);
        }
-       i++;
+	printf("thread join : %d\n",taskID[j]);
+       j++;
     }
     printf("Finished (process)\n");
     return 0;
--- mproc3.c
+++ mproc3.c
@@ -5,22 +5,23 @@
 #include <time.h>
 int count =0;
 pthread_mutex_t  mutex;
-void test_funct(int m_count){
-    printf("  COUNT : %d\n",m_count);
+void test_funct(int m_count,int m_n){
+    printf("  ID: %d   COUNT : %d  ",m_n,m_count);
     printf("2013136035 Hyeon Ji Ra\n");
+	
 
 }
 
 
-void* subproc(void* num){
-   
-   while(count< *(int*)num){
+void* subproc(void* id){
+   int  n = *(int*)id;
+   while(count< 20){
        srand(time(NULL));
        int r= rand()%10;
        sleep(r);
        pthread_mutex_lock(&mutex);
        count++;
-       test_funct(count);
+       test_funct(count,n);
        pthread_mutex_unlock(&mutex);
     }   
 }
@@ -28,13 +29,16 @@ void* subproc(void* num){
 int main()
 {
     int i=0;
+    int j=0;
     int res=0;
+    int taskID[10];
     pthread_mutex_init(&mutex, NULL);
     pthread_t p_thread[10];
     void *thread_result;
-    int num = 20;
+  
     while(i<10){
-        res = pthread_create(&p_thread[i], NULL,subproc,(void*)&num);
+        taskID[i] =i;
+        res = pthread_create(&p_thread[i], NULL,subproc,(void*)&taskID[i]);
         if(res !=0){
             perror("Thread creation failed");
             exit(EXIT_FAILURE);
@@ -42,14 +46,14 @@ int main()
 
         i++;
     }
-    i = 0;
-    while(i<10){
-       res =  pthread_join(p_thread[i], &thread_result);
+
+    while(j<10){
+       res =  pthread_join(p_thread[j], &thread_result);
        if(res !=0){
            perror("Thread creation failed");
            exit(EXIT_FAILURE);
        }
-       i++;
+       j++;
     }
     pthread_mutex_destroy(&mutex);
     printf("Finished Successfully\n");
